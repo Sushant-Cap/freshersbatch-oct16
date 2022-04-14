1fed3a15ffee2999b77f9357bdc489a79652a280
sbalukam <sushantkamble9284@gmail.com>
2022-04-14 12:54:52 +0530

    First commit

---
+++ SampleProj/src/com/sushant/Sample.java
@@ -0,0 +1,11 @@
+package com.sushant;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("hii this is 1st code");
+		
+		System.out.println("pushing source code to remote repo ");
+	}
+
+}
